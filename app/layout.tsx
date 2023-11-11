13566148856187002fde8c2cdf12af454fd58fbc
KHURRAM ALI <howtod543@gmail.com>
2023-11-11 15:04:42 +0500

    a

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ import { Analytics } from '@vercel/analytics/react';
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "KHURRAM | Personal Portfolio",
+  title: "KHURRAM | Portfolio",
   description: "KHURRAM is a full-stack developer with 2 years of experience.",
 };
 
