308d160c791d586fa8bb2899556d29a888fcc593
KHURRAM ALI <109402829+akhurram577@users.noreply.github.com>
2023-11-11 13:31:40 +0500

    new

--- components/intro.tsx
+++ components/intro.tsx
@@ -31,7 +31,7 @@ export default function Intro() {
             }}
           >
             <Image
-              src="/kh.jpg"
+              src="/kh.png"
               alt="Ricardo portrait"
               width="192"
               height="192"
